63ab166dac6f89c73d4775615bc0cebfedf33ee3
Serdar Günizi <Sgunizi@gmail.com>
2025-08-17 12:36:11 +0300

    fix: DProduction CORS hatası

---
+++ CopyCat/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages.tsx",
+    "./components.tsx",
+    "./App.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
