545fc8e9767f8b97f8ab0f5148ef253f3509cfef
Serdar Günizi <Sgunizi@gmail.com>
2025-09-23 23:40:53 +0300

    Revert "Fix python path issue for gunicorn"
    This reverts commit 6f351587ce9faaa79159eec6f946556f986c758c.

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -3,9 +3,6 @@ import multiprocessing
 # Server socket
 bind = "0.0.0.0:8000"
 
-# Change directory to the backend folder
-chdir = "backend"
-
 # Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
