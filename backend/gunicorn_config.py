4b22b5b47fc0687a004790cdc814b43f7afe2caf
Serdar Günizi <Sgunizi@gmail.com>
2025-09-23 23:22:40 +0300

    Add gunicorn for production

---
+++ backend/gunicorn_config.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:8000"
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# Logging
+accesslog = "-"
+errorlog = "-"
